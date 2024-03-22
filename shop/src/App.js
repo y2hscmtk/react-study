3b911cd71f05763662ae51f069bd2135415374f4
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-03-22 15:47:33 +0900

    새로운 프로젝트 생성 & Bootstrap 사용하기

---
+++ shop/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+import {Button,Container,Navbar,Nav} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className='App'>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Button variant="primary">Primary</Button>{' '}
+    </div>
+  );
+}
+
+export default App;
