f5a3f2ff57dea60ddcaa833087a3e8976fce92af
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-03-23 21:58:22 +0900

    이미지 넣는 법 & public 폴더 이용하기

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,21 +1,54 @@
 import './App.css';
 
+// bootstrap 라이브러리 사용전 import 필수
 import {Button,Container,Navbar,Nav} from 'react-bootstrap';
 
+// 이미지 사용을 위해~
+// bg_image_name => 사용할 이미지 변수명 
+import bg_image_name from './bg.png'
+
+
+// React Bootstrap에서 버튼 사용하기
 function App() {
   return (
     <div className='App'>
-      <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar bg="light" data-bs-theme="light">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#cart">Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <Button variant="primary">Primary</Button>{' '}
+      {/* 대문 */}
+      <div className='main-bg' style={{backgroundImage : 'url('+bg_image_name+')'}}>
+
+      </div>
+      {/* 상품 목록 */}
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-4'>
+            <img src='https://codingapple1.github.io/shop/shoes1.jpg' width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+            
+          <div className='col-md-4'>
+            <img src='https://codingapple1.github.io/shop/shoes2.jpg' width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className='col-md-4'>
+            <img src='https://codingapple1.github.io/shop/shoes3.jpg' width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+
+        </div>
+      </div>
+
+
     </div>
   );
 }
