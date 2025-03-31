bcaea7aa79702639f1197c2f63acd9994bd78df1
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2025-03-31 23:17:59 +0900

    페이지 라우팅 - 라우팅 설정하기

--- diary/src/App.jsx
+++ diary/src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Diary from './pages/Diary'
+import New from './pages/New'
+import NotFound from './pages/Notfound'
 
+// 1. "/" : 모든 일기를 조회하는 Home 페이지
+// 2. "/new" : 새로운 일기를 작성하는 New 페이지
+// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
-
   return (
-    <>
-    <h1>hello</h1>
-    </>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/new' element={<New />}></Route>
+      <Route path='/diary' element={<Diary />}></Route>
+      <Route path='*' element={<NotFound />}></Route>
+    </Routes>
+
   )
 }
 
---
+++ diary/src/pages/Notfound.jsx
@@ -0,0 +1,5 @@
+const NotFound = () => {
+    return <div>wrong page</div>
+}
+
+export default NotFound
