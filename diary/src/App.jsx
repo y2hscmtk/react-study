04e7e18fb487a28c045fb7f10ece27b3e6e8fab7
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2025-03-31 23:31:44 +0900

    페이지 라우팅 - 페이지 이동

--- diary/src/App.jsx
+++ diary/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link, useNavigate } from 'react-router-dom'
 import Home from './pages/Home'
 import Diary from './pages/Diary'
 import New from './pages/New'
@@ -9,13 +9,26 @@ import NotFound from './pages/Notfound'
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
+  const nav = useNavigate()
+  const onClickButton = () => {
+    nav("/new")
+  }
   return (
-    <Routes>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='/new' element={<New />}></Route>
-      <Route path='/diary' element={<Diary />}></Route>
-      <Route path='*' element={<NotFound />}></Route>
-    </Routes>
+    <>
+      <div>
+        <Link to={"/"}>Home </Link> 
+        <Link to={"/new"}>New </Link> 
+        <Link to={"/Diary"}>Diary </Link> 
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/new' element={<New />}></Route>
+        <Route path='/diary' element={<Diary />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
+      </Routes>
+    </>
+    
 
   )
 }
