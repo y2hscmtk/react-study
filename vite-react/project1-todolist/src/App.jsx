92f3e8fe020afefb1a2f3a529b6421447e64c69c
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2025-07-03 21:53:45 +0900

    🎨 8.4 Create - 투두 추가하기

--- vite-react/project1-todolist/src/App.jsx
+++ vite-react/project1-todolist/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
-import { useState } from 'react'
+import { useState,useRef } from 'react'
 import Header from './components/Header'
 import Edithor from './components/Edithor'
 import List from './components/List'
 
 // 더미데이터 객체 배열 정의
+// 매번 재생성 될 필요 없으므로 외부로 분리
 const mockData = [
   // 더미 데이터 생성용 객체 정의
   {
@@ -31,10 +32,24 @@ const mockData = [
 // state를 이용하는 모든 컴포넌트들의 조상 - 여기선 App컴포넌트 - 에 만들어야한다.
 function App() {
   const [todos, setTodos] = useState(mockData) // 더미데이터 정의
+  const idRef = useRef(3) // 아이디 설정용
+
+  const onCreate  = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime()
+    }
+    console.log("newTodo", newTodo)
+    // state 변수는 useState를 통해서만 변경 가능
+    setTodos([newTodo, ...todos]);
+    console.log(todos)
+  }
   return (
     <div className='App'>
       <Header/>
-      <Edithor/>
+      <Edithor onCreate={onCreate}/>
       <List/>
     </div>
   )
--- vite-react/project1-todolist/src/components/Edithor.jsx
+++ vite-react/project1-todolist/src/components/Edithor.jsx
@@ -1,10 +1,39 @@
 import "../Edithor.css"
+import { useState,useRef } from "react"
 
-const Edithor = () => {
+const Edithor = ({onCreate}) => {
+    const [content, setContent] = useState("")
+    const contentRef = useRef()
+    // 이벤트 헨들러 정의
+    const onChangeContent = (e) => {
+        setContent(e.target.value)
+    }
+
+    const onKeyDown = (e) => {
+        if(e.keyCode === 13){ // 엔터키 클릭시 입력 이벤트 동작
+            onSubmit()
+        }
+    }
+
+    const onSubmit = () => {
+        console.log("content = ", content)
+        if (content === ""){
+            contentRef.current.focus()
+            return;
+        } // 빈 문자열 저장 방지
+        onCreate(content)
+        setContent("")
+    }
+
     return (
         <div className="Edithor">
-            <input placeholder="새로운 Todo.." />
-            <button>추가</button>
+            <input
+                ref = {contentRef}
+                value={content}
+                onKeyDown={onKeyDown} // 사용자가 키보드 누를때 발동하는 이벤트 -> 엔터키 입력 구현
+                onChange={onChangeContent}
+                placeholder="새로운 Todo.." />
+            <button onClick={onSubmit}>추가</button>
         </div>
     )
 }
