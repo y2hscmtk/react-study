5b5830d44a8d53eca0604b57b4fa32d0faa7d830
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2025-07-14 22:51:26 +0900

    🎨 12.5 페이징 라우팅 - 동적 경로

--- vite-react/section12/src/pages/Diary.jsx
+++ vite-react/section12/src/pages/Diary.jsx
@@ -1,5 +1,11 @@
+import { useParams } from "react-router-dom"
+// URL 파라미터의 동적 경로를 가져올때 사용하는 훅
+
 const Diary = () => {
-    return <div>Diary</div>
+    const params = useParams()
+    console.log(params) // diary/1 과 같이 전달시, 1이 전달됨 -> 해당하는 아이디의 데이터를 얻어올수 있다.
+
+    return <div>{params.id} 번 일기입니다.</div>
 }
 
 export default Diary
--- vite-react/section12/src/pages/Home.jsx
+++ vite-react/section12/src/pages/Home.jsx
@@ -1,4 +1,11 @@
+import { useSearchParams } from "react-router-dom"
+
+// 쿼리스트링을 통한 값 전달을 useState 처럼 사용할 수 있다.(획득, 조작 가능)
+// locallhost:5173/?value=hello 와 같이 들어온 경우
+// params.get("value") 를 통해 hello 획득 가능
 const Home = () => {
+    const [params, setParams] = useSearchParams()
+    console.log(params.get("value"))
     return <div>Home</div>
 }
 
